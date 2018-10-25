13cad63a2db7906300660ae47a6d85df93d153a1
Alejandro Ortiz <alejandro.ortiz@unosquare.com>
2018-10-25 18:14:04 -0500

    first

--- SumDigits2/Program.cs
+++ SumDigits2/Program.cs
@@ -6,7 +6,25 @@ namespace SumDigits2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Ingrese un numero");
+            string a = Console.ReadLine();
+            int sum = 0;
+            int n;
+
+            foreach (char num in a)
+            {
+
+                n = Int32.Parse(num + "");
+
+                sum = sum + n;
+
+            }
+
+            Console.WriteLine("Suma= ", sum);
+
+            Console.ReadKey();
+
         }
     }
 }
